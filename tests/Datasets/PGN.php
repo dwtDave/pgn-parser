93b8a991a02a5ca2a3138db1188fb962e1cb6252
HueHue <dev@web-hl.de>
2025-05-13 14:06:20 +0200

    removed empty line

--- tests/Datasets/PGN.php
+++ tests/Datasets/PGN.php
@@ -13,4 +13,3 @@ dataset('valid_full_pgn', [
 		1. e4 c5 2. Nf3 d6 3. d4 cxd4 4. Nxd4 Nf6 5. Nc3 a6 6. Be2 e6 7. O-O Be7 8. f4 O-O 9. Kh1 Nc6 10. Be3 Qc7 11. a4 Bd7 12. Qe1 Nxd4 13. Bxd4 Bc6 14. Bd3 Nd7 15. Qg3 g6 16. f5 exf5 17. exf5 Ne5 18. Rae1 Rae8 19. Bxe5 dxe5 20. fxg6 hxg6 21. Bxg6 fxg6 22. Qxg6+ Kh8 
 		PGN,
 ]);
-
